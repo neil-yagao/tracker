52bfd1e335ed782cb90da7c6f124782787d11f1f
neil hu <neil.hu68@yahoo.com>
2016-12-08 17:08:07 +0800

    rename VUE

--- src/webapp/src/components/movements/MovementGeneral.vue
+++ src/webapp/src/components/movements/MovementGeneral.vue
@@ -1,8 +1,8 @@
 <template>
     <div id="movement-general">
         <ul class="nav nav-tabs nav-justified" style="max-width:600px">
-          <li role="presentation" :class="{'active': _.endsWith($route.path, 'movements') }"><a href="#/lift/movements">浏览动作</a></li>
-          <li role="presentation" :class="{'active': _.endsWith($route.path, 'new-movement') }"><a href="#/lift/movements/new-movement">未找到？自己加一个!</a></li>
+          <li role="presentation" :class="{'active': $route.path.indexOf('movements') > -1}"><a href="#/lift/movements">浏览动作</a></li>
+          <li role="presentation" :class="{'active': $route.path.indexOf('new-movement')> -1 }"><a href="#/lift/movements/new-movement">未找到？自己加一个!</a></li>
         </ul>
         <router-view class="container-fluid" style="margin-top:0.5em"></router-view>
     </div>
