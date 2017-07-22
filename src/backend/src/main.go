1431c6a8292c349e42b49d254ecba6bd03aedff1
neil hu <neil.hu68@yahoo.com>
2017-07-22 10:01:36 +0800

    add config db option

--- src/backend/src/main.go
+++ src/backend/src/main.go
@@ -1,6 +1,6 @@
 package main
 
-import (
+/*import (
 	"github.com/astaxie/beego"
 	_ "routers"
 )
@@ -8,9 +8,9 @@ import (
 func main() {
 
 	beego.Run()
-}
+}*/
 
-/*import (
+import (
 	"github.com/astaxie/beego/orm"
 	_ "models"
 )
@@ -18,4 +18,4 @@ func main() {
 func main() {
 
 	orm.RunCommand()
-}*/
+}
--- src/backend/src/models/models_init.go
+++ src/backend/src/models/models_init.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"github.com/astaxie/beego/config"
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -9,7 +10,14 @@ func init() {
 	orm.Debug = true
 	orm.RegisterDriver("mysql", orm.DRMySQL)
 
-	orm.RegisterDataBase("default", "mysql", "root:password@tcp(127.0.0.1:3306)/powerlift?charset=utf8")
+	config, err := config.NewConfig("ini", "conf/db.conf")
+	if err != nil {
+		panic(err)
+	}
+	user := config.String("username")
+	password := config.String("password")
+	connectString := user + ":" + password + "@tcp(127.0.0.1:3306)/powerlift?charset=utf8"
+	orm.RegisterDataBase("default", "mysql", connectString)
 
 	orm.RegisterModel(new(UserInfo), new(Movement), new(AssignedPlan), new(UserSession),
 		new(SessionWorkout), new(Exercise), new(SessionMovement), new(Session), new(Plan), new(PhysiqueInfo))
