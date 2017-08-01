494ba00fe91ccf5e89eaab78dd13c5b3080ed28d
neil hu <neil.hu68@yahoo.com>
2017-08-01 17:37:25 +0800

    add history page

---
+++ src/web-mobile/src/components/movement/movement-history.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+    <md-dialog ref="movement-history" style="max-height:35em" @close="closing()">
+        <md-dialog-title>动作历史</md-dialog-title>
+        <md-dialog-content>
+            
+        </md-dialog-content>
+    </md-dialog>
+</div>
+</template>
+<script>
+</script>
